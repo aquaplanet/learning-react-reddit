66b44afa53e7818af4ab5efa4ac4ce86b0d06a5b
aquaplanet <anders.per.andersson+github@gmail.com>
2021-04-19 20:36:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-1w31wr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-1w31wr)
---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import RedditPosts from "RedditPosts";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div id="react">
+      <header>
+        <h1>Anders Reddit application</h1>
+        <p>
+          Man skulle vilja ha ett bra webbprojekt för att läsa JSON och sådant,
+          men jag kommer inte på något. Kanske att läsa Reddit-APIt och göra
+          roliga saker med det men ... vänta det var faktiskt en bra idé
+        </p>
+      </header>
+
+      <main>
+        <h1>Posts from r/MechanicalKeyboards</h1>
+        <RedditPosts />
+      </main>
+
+      <footer>
+        <p>Detta är sidfoten</p>
+      </footer>
+    </div>
+  );
+}
