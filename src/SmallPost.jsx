5999719306fdc0cb5a4be7484d61d97dbf31bf06
andan48 <anders.p.andersson@vgregion.se>
2021-05-10 10:27:45 +0200

    Ändringar till styles och SmallPost.

--- src/SmallPost.jsx
+++ src/SmallPost.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import "./SmallPost.css"
 
 let SmallPost = function () {
   return (
-  <div>
-    <img src="https://placekitten.com/50/80"/>
-    <h2>Rubrik</h2>
-    <h3>Undertext</h3>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
-  </div>);
+      <div className="smallpost">
+        <img src="https://placekitten.com/50/80" alt="Text till bilden"/>
+        <h2>Rubrik</h2>
+        <h3>Undertext</h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+      </div>);
 };
 
 export default SmallPost
